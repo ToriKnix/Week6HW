8a15db20edbc3ebcb8b7e941a307f27401d3ffe2
ToriKnix <toriknixmusic@gmail.com>
2023-05-13 17:32:14 -0600

    fixed html and other bugs. still trying to get this functioning. redoing CSS.

--- Week6HW/script.js
+++ Week6HW/script.js
@@ -1,8 +1,7 @@
-// Define variables to store the API endpoint and your API key
+
 const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
 const apiKey = "36f9850102a58e16f2699ebeb0e51d39";
 
-// Define a function to fetch weather data from the API
 function getWeatherData(city) {
 
   fetch(`${apiEndpoint}?q=${city}&appid=${apiKey}&units=metric`)
@@ -25,12 +24,12 @@ function getWeatherData(city) {
       const windSpeedElement = document.getElementById("wind-speed");
       windSpeedElement.textContent = `Wind speed: ${windSpeed} km/h`;
     })
-    // Handle errors
+  
     .catch(error => {
       console.error("Error fetching weather data:", error);
     });
 }
 
-// Call the getWeatherData() function with the desired location as the argument
+
 getWeatherData("New York");
 getWeatherData("Los Angeles");
